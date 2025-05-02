03b52adea379f32bbb2a93f1d6261e53082c42e0
steveski <steve@steve-york.com>
2025-05-02 22:42:15 +1000

    feat: added favicon.ico

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <script lang="ts" setup>
+//import { useFavicon } from '@vueuse/core';
 import VoteButton from '@/components/VoteButton.vue';
+
+//useFavicon('/favicon.ico');
+// const favicon = useFavicon('/favicon.ico');
+// favicon.value = '/path to new icon';
 </script>
 
 <template>
